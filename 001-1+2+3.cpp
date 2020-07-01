506cc55cf12c724e5079d68ca3e16cf5d2a55754
gidital <184659@gmail.com>
2020-07-01 21:13:57 +0900

    new file

---
+++ 001-1+2+3.cpp
@@ -0,0 +1,9 @@
+/*#include <stdio.h>
+
+int onetwothr(int num)
+{
+	if(num == 1) return 1;
+	if(num == 2) return 2;
+	if(num == 3) return 4;
+	else return onetwothr(num-1) + onetwothr(num-2) + onetwothr(num-3);
+}*/
