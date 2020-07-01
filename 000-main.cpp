0690b8e55ff97085f42697413ae0d060ff84479a
gidital <184659@gmail.com>
2020-07-01 21:21:59 +0900

    main()

---
+++ 000-main.cpp
@@ -0,0 +1,163 @@
+#include <stdio.h>
+
+//void onetwothr(int num); // 1463
+//int n2_tile(int width); // 11726
+//void init_rgb(); // 1149
+//long long pinary(); // 2193
+//int conssum(); // 1912
+//void init_easystr(); // 10844
+//void n2_tile2(); // 11727
+//void init_incarr(); // 11053
+//long buycard(); // 11052
+//void wave(); // 9461
+//void init_leave(); // 14501
+//void choco(); // 2163
+//void init_bridge(); // 1010
+//void coin1(); // 2293
+//void climb(); // 2579
+//void hillarr(); // 11057
+//void sqrsum(); // 1699
+//void bigarr(); // 11055
+//void arrsum(); // 2167
+//void coin2(); // 2294
+//void maze(); // 11048
+//void lcs(); // 9251
+//void n3_tile(); // 2133
+//void binomial(); // 11051
+//void tile01(); // 1904
+//void decarr(); // 11722
+//void bitonic(); // 11054
+//void init_downhill(); // 1520
+//void init_jump(); // 1890
+//void partsum(); // 2225
+//void init_panda(); // 1937
+//void largeSqr(); // 1915
+//void init_bwc(); // 2647
+//void candy(); // 4781
+//void comb(); // 11066
+//void down(); // 2096
+//void lcs2(); // 9252
+//void palin(); // 10942
+//void passcord(); // 2011
+//void chainmatx(); // 11049
+//void grade1(); //5557
+//void tribble(); // 9507
+//void init_route(); // 10164
+//void lineup(); // 2631
+//void tsp(); // 2098
+//void guitar(); // 1495
+//void seat(); // 2302
+//void jumpjump(); // 11060
+//void coin(); // 9084
+//void decNum(); // 1038
+//void lcCs(); // 5582
+//void robot(); // 2169
+//void architect(); // 2352
+//void plumtree(); // 2240
+//void app(); // 7579
+//void dict(); // 1256
+//void divPalin(); // 1509
+//void cardGame(); // 10835
+//void lcs3(); // 1958
+//void skyscrap(); // 1328
+//void police(); // 2618
+//void stairNum(); // 1562
+//void SNS(); // 2533
+//void twoarm(); // 2629
+//void makePalin(); // 1254
+//void dontShrink(); // 2688
+//void stoneBridge(); // 2602
+//void change(); // 2624
+//void heilstone(); // 3943
+//void colorBand(); // 2482
+//void smallTrain(); // 2616
+//void view(); // SWEA 1206
+//void closerBit(); // 2698
+//void plant(); // 1102
+//void n2Tile_2(); // 1793
+//void strangeP(); // SWEA 4112
+//void cupboard(); // 2666
+//void optimizeR(); // SWEA 1247
+//void infiniteNum(); // 1351
+//void krii(); // 11058
+//void HsDict(); // SWEA 3135
+//void seperate(); // 2228
+//void triGraph(); // 4883
+//void danceRev(); // 2342
+//void validity(); // SWEA 10059
+//void attendance(); // 1563
+//void passcode(); // SWEA 1242
+//void invest(); // 2662
+//void captain(); // 1660
+//void game(); // 1103
+//void bulb(); // 2449
+//void exponen(); // 2410
+
+// 문제 찾을때 줄 번호 -3 하면 해당 문제 파일이 나옴 
+// SWEA = SAMSUNG S/W EXPERT ACADEMY 문제, 미표기한 소스는 BOJ = BAEKJOON ONLINE JUDGE 문제 
+int main()
+{
+	int test= 1, i;
+	//scanf("%d", &test);
+	for(i= 0; i< test; i++) {
+		//comb();
+		//down();
+		//lcs2();
+		//palin();
+		//passcord();
+		//chainmatx();
+		//grade1();
+		//tribble();
+		//init_route();
+		//lineup();
+		//tsp();
+		//guitar();
+		//seat();
+		//jumpjump();
+		//coin();
+		//decNum();
+		//lcCs();
+		//robot();
+		//architect();
+		//plumtree();
+		//app();
+		//dict();
+		//divPalin();
+		//cardGame();
+		//lcs3();
+		//skyscrap();
+		//police();
+		//stairNum();
+		//SNS();
+		//twoarm();
+		//makePalin();
+		//dontShrink();
+		//stoneBridge();
+		//change();
+		//heilstone();
+		//colorBand();
+		//smallTrain();
+		//view(); 
+		//closerBit();
+		//plant();
+		//n2Tile_2();
+		//strangeP();
+		//cupboard();
+		//optimizeR();
+		//infiniteNum();
+		//krii();	
+		//HsDict();
+		//seperate();
+		//triGraph();
+		//danceRev();
+		//validity();
+		//attendance();
+		//passcode();
+		//invest();
+		//captain();
+		//game();
+		//bulb();
+		//exponen();
+	}
+	return 0;
+}
